979b9dbf1c609fd4d72372aafc61620a27c9d875
FacundoArrieta <facundo_04@hotmail.com>
2020-02-06 15:52:21 -0300

    ejercicios segunda clase

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -10,8 +10,4 @@ if( edad >= 18){
     alert("Es mayor de edad");
     } 
 
-else
-{
-alert("Es menor de edad");
-} 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -5,14 +5,13 @@ var edad;
 
 edad = (document.getElementById("edad").value);
 
-if( edad < 18){
+if( edad >= 18){
 
-    alert("Es menor de edad");
+    alert("Es mayor de edad");
     } 
-    
-if( edad > 18){
 
-        alert("Es mayor de edad");
-        }     
-
+else
+{
+alert("Es menor de edad");
+} 
 }//FIN DE LA FUNCIÓN
